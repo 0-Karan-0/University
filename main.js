57a300d996891039903e88b8548d5f5744185b73
0-Karan-0 <94389978+0-Karan-0@users.noreply.github.com>
2021-12-21 12:22:35 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+var navLinks = document.getElementById("navLinks");
+        function showMenu(){
+            navLinks.style.right = "0";
+        }
+        function hideMenu(){
+            navLinks.style.right = "-200px";
+        }
+
+
+mediumZoom('.zoom',{
+    background: transparent,
+    scrollOFFset: 200
+    margin: 50,
+
+})
